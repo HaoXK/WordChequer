cf6f3e11b898e1c56f4efd7478b4f8b4fc548107
HaoXK <haoxuank@nycstudents.net>
2024-10-30 12:29:53 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,5 +12,13 @@ public class Main
     WordChecker x = new WordChecker(words);
     System.out.println(x.isWordChain());
     System.out.println(x.createList("to"));
+    ArrayList<String> wordsv2 = new ArrayList<String>();
+    wordsv2.add("an");
+    wordsv2.add("band");
+    wordsv2.add("band");
+    wordsv2.add("abandon");
+    System.out.println(wordsv2);
+    WordChecker y = new WordChecker(wordsv2);
+    System.out.println(y.isWordChain());
     }
 }
